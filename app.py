318a79dc403735b34698cf93aa621170f385d45a
aravindm-am <aravind.menon@subex.com>
2025-05-28 15:01:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -476,7 +476,6 @@ with tabs[0]:
     uploaded_file = st.file_uploader("Choose a CSV file", type=["csv"], key="batch_upload")
     if uploaded_file is not None:
         df_uploaded = pd.read_csv(uploaded_file)        
-        st.dataframe(df_uploaded.head())
         if st.button("Score", key="score_batch_button"):
             # Upload the file to the Databricks workspace directory
             workspace_path = f"/Workspace/Users/aravind.menon@subex.com/Spam Detection/{uploaded_file.name}"
